cf7d4b9ff2014ce364dcecdba95cf84fa86969a8
josselyne <>
2020-12-06 23:01:57 -0500

    inicializacion proyecto

---
+++ src/fruteria/Fruteria.java
@@ -0,0 +1,34 @@
+/*
+ * 4.	Una frutería ofrece las manzanas con descuento según la siguiente tabla:
+		NUM. DE KILOS COMPRADOS		% DESCUENTO
+			  0      -   2					  0%
+			  2.01 -   5 					10%
+			  5.01 - 10					15%
+			10.01 en adelante				20%
+
+Determinar cuánto pagara una persona que compre manzanas es esa frutería.
+
+ */
+package fruteria;
+
+import java.util.Scanner;
+
+/**
+ *
+ * josselyne ester chilito galindez cod: 20201187434
+ */
+public class Fruteria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner (System.in);
+        final int valorkilo=2000,nkilos;
+        System.out.println("ingrese cuantos kilos de manzana quiere comprar");
+        nkilos=entrada.nextInt();
+        
+        
+    }
+    
+}
